d64a083f9744777ba532e6cc32e69a16e9aeae7c
James Quinn <jamesquinn@Jamess-MacBook-Pro.local>
2026-05-27 20:00:26 -0400

    going live2

--- server.ts
+++ server.ts
@@ -243,10 +243,13 @@ async function startServer() {
   } else {
     const distPath = path.join(process.cwd(), "dist");
     app.use(express.static(distPath));
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(distPath, "index.html"));
-    });
-  }
+  //   app.get("*", (req, res) => {
+  //     res.sendFile(path.join(distPath, "index.html"));
+  //   });
+  // }
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+  });
 
   app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${PORT}`);
